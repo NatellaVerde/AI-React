f9bc07eff87c699707dc8652280c246c40c03281
NatellaVerde <xxxxnataliyaxxxx@gmail.com>
2025-09-21 08:59:07 -0600

    set up prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,17 +1,16 @@
-import express from 'express'
+import express from 'express';
 import type { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("hello!")
+app.get('/', (req: Request, res: Response) => {
+   res.send('hello!');
 });
-app.get("/api/hello", (req: Request, res: Response) => {
-    res.json({message: "world"})
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'world' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+   console.log(`Server is running on http://localhost:${port}`);
 });
-
