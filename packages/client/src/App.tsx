4a9c6ec5084deb57f708d5b5f61bb8496d3c9e59
NatellaVerde <xxxxnataliyaxxxx@gmail.com>
2025-09-23 14:22:19 -0600

    added styles to chat bot

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import ChatBot from './components/ui/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   });
-
    return (
-      <div className="p-4">
-         {message}
+      <div className="p-4 h-screen w-full">
          <ChatBot />
       </div>
    );
